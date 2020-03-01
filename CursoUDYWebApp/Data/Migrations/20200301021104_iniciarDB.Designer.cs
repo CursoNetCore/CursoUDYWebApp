12bc699f83e6bd8cdae2647006707608b5998680
adrianRadice <radiceadriann@gmail.com>
2020-02-29 23:29:26 -0300

    migration usr

--- CursoUDYWebApp/Data/Migrations/20200301021104_iniciarDB.Designer.cs
+++ CursoUDYWebApp/Data/Migrations/20200301021104_iniciarDB.Designer.cs
@@ -4,14 +4,16 @@ using CursoUDYWebApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CursoUDYWebApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200301021104_iniciarDB")]
+    partial class iniciarDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
